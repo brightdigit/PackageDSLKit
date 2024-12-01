4fe7de316799159523e0f77b6129a3c49d5b447e
Leo Dion <leogdion@brightdigit.com>
2024-11-30 19:14:49 -0500

    fixing code

---
+++ Tests/PackageDSLKitTests/Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  PackageDSLKit
+//
+//  Created by Leo Dion on 11/30/24.
+//
+
+import Testing
+
+internal struct Test {
+  @Test internal func firstTest() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+}
