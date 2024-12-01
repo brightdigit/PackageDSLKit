73f6d91c0167d9dcf1b1fa1ce83af9f4b1d2bf06
leogdion <leogdion@brightdigit.com>
2024-11-30 19:40:04 -0500

    Setting up Swift Package (#1)

---
+++ Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// swiftlint:disable explicit_acl explicit_top_level_acl
+
+import PackageDescription
+
+let package = Package(
+  name: "PackageDSLKit",
+  platforms: [
+    .macOS(.v10_15)
+  ],
+  products: [
+    .library(name: "PackageDSLKit", targets: ["PackageDSLKit"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0-prerelease-2024-11-18")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "PackageDSLKit"
+    ),
+    .executableTarget(
+      name: "package",
+      dependencies: [
+        .product(name: "SwiftSyntax", package: "swift-syntax"),
+        .product(name: "SwiftParser", package: "swift-syntax"),
+      ]
+    ),
+    .testTarget(
+      name: "PackageDSLKitTests"
+    )
+  ]
+)
+
+// swiftlint:enable explicit_acl explicit_top_level_acl
